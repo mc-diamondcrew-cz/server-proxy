0c42ab1ee76f58b67fe9e2768217124e622ac26a
ThePlay3r <radomil.janicek@gmail.com>
2022-02-14 13:06:32 +0100

    chore: better locale downloading

--- language.sh
+++ language.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-declare -A LANGUAGES=(
-  []=
+declare -a LANGUAGES=(
+  ""
 )
 
-for lang in "${!LANGUAGES[@]}"
+for lang in "${LANGUAGES[@]}"
 do
-  echo "Downloading language ${LANGUAGES[$lang]}.."
-  (cd "${lang//./}" || return ; curl -O "${LANGUAGES[$lang]}")
+  echo "Downloading language ${lang}.."
+  (cd plugins/"${lang%\/*}" || exit ; curl -O https://raw.githubusercontent.com/mc-diamondcrew-cz/translates/master/"${lang}")
 done
